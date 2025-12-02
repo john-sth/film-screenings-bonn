7e98730f40a0e67cfa6ff4dc02488fcb1aa55440
alex <alexdot1@protonmail.com>
2025-12-03 00:13:13 +0100

    fetch titles and corresponding screenings from neue filmbühne bonn

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let document = scraper::Html::parse_document(&body);
     //println!("body: {}", body);
 
+    //===========================================================
+    // filter after the css selectors
+    // currently title is inside an a tag, which itself
+    // is in a h2 tag
+    //===========================================================
+    let block_selector = Selector::parse(".nfb_liste").unwrap();
     let selector = Selector::parse("h2 > a").unwrap();
+    let date_selector = Selector::parse(".vorstellung_text").unwrap();
 
-    for element in document.select(&selector) {
-        let title = element.text().collect::<String>().trim().to_string();
-        println!("Film Title: {}", title.trim());
+    //===========================================================
+    // iterate through the whole document and print the movie titles
+    //===========================================================
+    // get the text nodes of the element, join them to a String
+    // Object and trim for whitespaces
+    // trim returns a &str slice, which borrows the original String
+    // trim doesnt own the data
+    // to_string allocates a new String and copies the slice into it
+    //===========================================================
+
+    let movie_data = document.select(&block_selector);
+
+    for movie in movie_data {
+        let title = movie
+            .select(&selector)
+            .next()
+            .map(|d| d.text().collect::<String>().trim().to_string())
+            .unwrap();
+        println!("Film Title: {}", title);
+        let screenings = movie.select(&date_selector);
+        for screening in screenings {
+            let text = screening.text().collect::<String>().trim().to_string();
+            println!("screening: {}", text);
+        }
     }
     Ok(())
 }
