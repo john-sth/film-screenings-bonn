46f192c979659ddd63d5649d0f394375e168fa57
alex <alexdot1@protonmail.com>
2025-12-02 21:07:50 +0100

    init

---
+++ target/debug/deps/film_screenings_bonn-21bc5210882571b8.d
@@ -0,0 +1,5 @@
+/home/john/film-screenings-bonn/target/debug/deps/film_screenings_bonn-21bc5210882571b8.d: src/main.rs
+
+/home/john/film-screenings-bonn/target/debug/deps/libfilm_screenings_bonn-21bc5210882571b8.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/film_screenings_bonn-cc333218dcef415a.d
@@ -0,0 +1,5 @@
+/home/john/film-screenings-bonn/target/debug/deps/film_screenings_bonn-cc333218dcef415a.d: src/main.rs
+
+/home/john/film-screenings-bonn/target/debug/deps/libfilm_screenings_bonn-cc333218dcef415a.rmeta: src/main.rs
+
+src/main.rs:
